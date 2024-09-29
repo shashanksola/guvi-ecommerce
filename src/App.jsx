3402ddeb7a9bb0e6115f044784122a0592e413da
shashanksola <solashashank1010@gmail.com>
2024-09-29 19:12:11 +0530

    added navbar -- need to make it mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
     <>
-      <div className="p-6">
-        Hello
-      </div>
+      <Navbar />
+      <hr />
     </>
   )
 }
